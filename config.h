46a439f583f69c3501cd30875b1808c24c2a89f7
DbpGaming <LbpDbpGaming@gmail.com>
2021-09-11 11:28:40 -0400

    gruvbox

--- config.h
+++ config.h
@@ -7,15 +7,14 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_bg[]       = "#282828";
+static const char col_brdr[]       = "#928374";
+static const char col_text[]       = "#ebdbb2";
+static const char col_sel[]        = "#fb4934";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_text, col_bg, col_brdr },
+	[SchemeSel]  = { col_text, col_sel,  col_sel  },
 };
 
 /* tagging */
